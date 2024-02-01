cc20f995f5eac1f7a807ffe1725fcceb4c0871eb
Orion777-cmd <abiy.biru78@gmail.com>
2024-02-01 22:05:18 +0300

    Add skeleton of music and user resources

--- music-backend/src/app.module.ts
+++ music-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MusicModule } from './music/music.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MusicModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
